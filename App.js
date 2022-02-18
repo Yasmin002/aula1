c5104ffd524baf96d3f0593c7bbec3478ed77bef
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-18 18:46:07 -0300

    Inicio do primeiro projeto de pam

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Olá mundo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    color: '#00FF7F',
+    fontSize:30
+  }
 });
